461168c65153f0c8980fa3449dc672879558b8b8
cherryan613 <cherry613@ewhain.net>
2025-04-08 15:06:49 +0900

    ami

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_key_pair" "ec2_key" {
 
 # EC2 인스턴스 생성
 resource "aws_instance" "web_server" {
-  ami             = "ami-086cae3329a3f7d75" # Amazon Linux 2 AMI ID
+  ami             = "ami-062cddb9d94dcf95d"
   instance_type   = "t2.micro"
   key_name        = aws_key_pair.ec2_key.key_name
   security_groups = [aws_security_group.web_sg.name]
